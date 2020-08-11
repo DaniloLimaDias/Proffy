7f02fce5af09f606decc5393c693af86b6489cea
danilo Lima Dias <danilo743377@gmail.com>
2020-08-11 12:03:15 -0300

    Plataforma Profft concluida e funcional. Evento nlw

---
+++ src/database/fake_data.js
@@ -0,0 +1,24 @@
+//Dados
+const proffys = [{
+  name: "Diego Fernandes",
+  avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+  whatsaap: "12345678",
+  bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e pormudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+  subject: "Química",
+  cost: "20",
+  weekday: [0],
+  time_from: [720],
+  time_to: [1220]
+},
+{
+  name: "Danilo Lima",
+  avatar: "https://avatars2.githubusercontent.com/u/62615069?s=460&u=9771b44c5a716a402116008a2cbda7e8c9df870d&v=4",
+  whatsaap: "9999999",
+  bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e pormudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+  subject: "Química",
+  cost: "20",
+  weekday: [1],
+  time_from: [720],
+  time_to: [1220]
+}
+]
